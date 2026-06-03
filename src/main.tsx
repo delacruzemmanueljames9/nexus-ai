0f315e6f0e7dd8ffd6c7109ed121b08054e4d325
delacruzemmanueljames9 <delacruzemmanueljames9@gmail.com>
2026-06-03 10:08:01 +0800

    Wrap App component with providers in StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AuthProvider } from "@/context/AuthContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
